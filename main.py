c47b97fc08894b247eef79929d1b695ce4e59e51
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2020-11-19 13:22:42 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-tempf = 60
+tempf = 70
+#while True:
+    #print("temperature " + input.temperature(TemperatureUnit.FAHRENHEIT))
+#if input.temperature(TemperatureUnit.FAHRENHEIT) > tempf:
+    #light.set_pixel_color(5, light.rgb(250, 0, 0)) 
+#else:
+    #light.clear()
+
 while True:
-    print("temperature " + input.temperature(TemperatureUnit.FAHRENHEIT))
-if input.temperature(TemperatureUnit.FAHRENHEIT) > tempf:
-    light.set_pixel_color(5, light.rgb(250, 0, 0)) 
-else:
-    light.clear()
+    print("temperature " + input.temperature(TemperatureUnit.FAHRENHEIT) + "F")
+if input.temperature(TemperatureUnit.FAHRENHEIT) < 70 + input.temperature(TemperatureUnit.FAHRENHEIT) < 40:
+    light.set_pixel_color(5, light.rgb(0, 250, 0))
--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
-let tempf = 60
+let tempf = 70
+// while True:
+// print("temperature " + input.temperature(TemperatureUnit.FAHRENHEIT))
+// if input.temperature(TemperatureUnit.FAHRENHEIT) > tempf:
+// light.set_pixel_color(5, light.rgb(250, 0, 0)) 
+// else:
+// light.clear()
 while (true) {
-    console.log("temperature " + input.temperature(TemperatureUnit.Fahrenheit))
+    console.log("temperature " + input.temperature(TemperatureUnit.Fahrenheit) + "F")
 }
-if (input.temperature(TemperatureUnit.Fahrenheit) > tempf) {
-    light.setPixelColor(5, light.rgb(250, 0, 0))
-} else {
-    light.clear()
+if (input.temperature(TemperatureUnit.Fahrenheit) < 70 + input.temperature(TemperatureUnit.Fahrenheit) && 70 + input.temperature(TemperatureUnit.Fahrenheit) < 40) {
+    light.setPixelColor(5, light.rgb(0, 250, 0))
 }
 
