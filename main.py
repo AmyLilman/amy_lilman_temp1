7bd71912bfdeba97be490dd9780a9364ae26d586
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2020-11-19 13:00:35 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-temp = 60
-if temp > 60
-    
+temp = 70
+if temp > 60:
+   light.set_pixel_color(5, light.rgb(250, 0, 0)) 
+elif temp < 70 + temp > 40:
+    light.set_pixel_color(5, light.rgb(0, 220, 0))
+else:
+    light.set_pixel_color(5, light.rgb(0, 0, 250))
--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-let temp = 60
+let temp = 70
+if (temp > 60) {
+    light.setPixelColor(5, light.rgb(250, 0, 0))
+} else if (temp < 70 + temp && 70 + temp > 40) {
+    light.setPixelColor(5, light.rgb(0, 220, 0))
+} else {
+    light.setPixelColor(5, light.rgb(0, 0, 250))
+}
+
