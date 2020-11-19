e44dfb01bd99c8209a5efc69875bd2e9e3eed65a
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2020-11-19 13:13:00 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-temp = 70
+tempf = 60
 while True:
     print("temperature " + input.temperature(TemperatureUnit.FAHRENHEIT))
-if input.temperature(TemperatureUnit.FAHRENHEIT) > 60:
+if input.temperature(TemperatureUnit.FAHRENHEIT) > tempf:
     light.set_pixel_color(5, light.rgb(250, 0, 0)) 
-elif input.temperature(TemperatureUnit.FAHRENHEIT) < 70 + input.temperature(TemperatureUnit.CELSIUS) > 40:
-    light.set_pixel_color(5, light.rgb(0, 220, 0))
 else:
-    light.set_pixel_color(5, light.rgb(0, 0, 250))
+    light.clear()
--- main.ts
+++ main.ts
@@ -1,12 +1,10 @@
-let temp = 70
+let tempf = 60
 while (true) {
     console.log("temperature " + input.temperature(TemperatureUnit.Fahrenheit))
 }
-if (input.temperature(TemperatureUnit.Fahrenheit) > 60) {
+if (input.temperature(TemperatureUnit.Fahrenheit) > tempf) {
     light.setPixelColor(5, light.rgb(250, 0, 0))
-} else if (input.temperature(TemperatureUnit.Fahrenheit) < 70 + input.temperature(TemperatureUnit.Celsius) && 70 + input.temperature(TemperatureUnit.Celsius) > 40) {
-    light.setPixelColor(5, light.rgb(0, 220, 0))
 } else {
-    light.setPixelColor(5, light.rgb(0, 0, 250))
+    light.clear()
 }
 
