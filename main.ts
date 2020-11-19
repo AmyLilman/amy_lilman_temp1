9dbbc518e3bb846c5d287d42beaabe9ad7fb3318
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2020-11-19 13:30:03 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -9,6 +9,26 @@ while (true) {
     console.log("temperature " + input.temperature(TemperatureUnit.Fahrenheit) + " F")
 }
 if (input.temperature(TemperatureUnit.Fahrenheit) < 70 + input.temperature(TemperatureUnit.Fahrenheit) && 70 + input.temperature(TemperatureUnit.Fahrenheit) > 40) {
+    light.setPixelColor(0, light.rgb(0, 250, 0))
+    light.setPixelColor(1, light.rgb(0, 250, 0))
+    light.setPixelColor(2, light.rgb(0, 250, 0))
+    light.setPixelColor(3, light.rgb(0, 250, 0))
+    light.setPixelColor(4, light.rgb(0, 250, 0))
     light.setPixelColor(5, light.rgb(0, 250, 0))
+    light.setPixelColor(6, light.rgb(0, 250, 0))
+    light.setPixelColor(7, light.rgb(0, 250, 0))
+    light.setPixelColor(8, light.rgb(0, 250, 0))
+    light.setPixelColor(9, light.rgb(0, 250, 0))
+} else {
+    light.setPixelColor(0, light.rgb(0, 0, 250))
+    light.setPixelColor(1, light.rgb(0, 0, 250))
+    light.setPixelColor(2, light.rgb(0, 0, 250))
+    light.setPixelColor(3, light.rgb(0, 0, 250))
+    light.setPixelColor(4, light.rgb(0, 0, 250))
+    light.setPixelColor(5, light.rgb(0, 0, 250))
+    light.setPixelColor(6, light.rgb(0, 0, 250))
+    light.setPixelColor(7, light.rgb(0, 0, 250))
+    light.setPixelColor(8, light.rgb(0, 0, 250))
+    light.setPixelColor(9, light.rgb(0, 0, 250))
 }
 
