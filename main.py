e3e567cd874a8525948d09ea323a271fa9e3c947
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2020-11-19 13:08:50 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 temp = 70
-if temp > 60:
-   light.set_pixel_color(5, light.rgb(250, 0, 0)) 
-elif temp < 70 + temp > 40:
+while True:
+    print("temperature" + input.temperature(TemperatureUnit.FAHRENHEIT))
+if input.temperature(TemperatureUnit.FAHRENHEIT) > 60:
+    light.set_pixel_color(5, light.rgb(250, 0, 0)) 
+elif input.temperature(TemperatureUnit.FAHRENHEIT) < 70 + input.temperature(TemperatureUnit.CELSIUS) > 40:
     light.set_pixel_color(5, light.rgb(0, 220, 0))
 else:
     light.set_pixel_color(5, light.rgb(0, 0, 250))
--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 let temp = 70
-if (temp > 60) {
+while (true) {
+    console.log("temperature" + input.temperature(TemperatureUnit.Fahrenheit))
+}
+if (input.temperature(TemperatureUnit.Fahrenheit) > 60) {
     light.setPixelColor(5, light.rgb(250, 0, 0))
-} else if (temp < 70 + temp && 70 + temp > 40) {
+} else if (input.temperature(TemperatureUnit.Fahrenheit) < 70 + input.temperature(TemperatureUnit.Celsius) && 70 + input.temperature(TemperatureUnit.Celsius) > 40) {
     light.setPixelColor(5, light.rgb(0, 220, 0))
 } else {
     light.setPixelColor(5, light.rgb(0, 0, 250))
