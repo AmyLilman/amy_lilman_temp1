c317b44c6edf427182e2803ba2cdc70d1681c00d
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2020-11-20 23:39:09 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -8,7 +8,9 @@
 while (true) {
     console.log("temperature " + input.temperature(TemperatureUnit.Fahrenheit) + " F")
 }
-if (input.temperature(TemperatureUnit.Fahrenheit) < 70 + input.temperature(TemperatureUnit.Fahrenheit) && 70 + input.temperature(TemperatureUnit.Fahrenheit) > 40) {
+if (input.temperature(TemperatureUnit.Fahrenheit) > 70) {
+    light.setPixelColor(5, light.rgb(250, 0, 0))
+} else if (input.temperature(TemperatureUnit.Fahrenheit) < 70 + input.temperature(TemperatureUnit.Fahrenheit) && 70 + input.temperature(TemperatureUnit.Fahrenheit) > 40) {
     light.setPixelColor(0, light.rgb(0, 250, 0))
     light.setPixelColor(1, light.rgb(0, 250, 0))
     light.setPixelColor(2, light.rgb(0, 250, 0))
